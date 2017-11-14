302fc07529ace7cfd678ce7d036d8347f163c5bf
davemetocean <dave@ngaru.rag.metocean.co.nz>
2017-11-14 14:43:30 +1300

    Working version of dataset plugin

--- kong/plugins/dataset-access/access.lua
+++ kong/plugins/dataset-access/access.lua
@@ -1,11 +1,18 @@
+local responses = require "kong.tools.responses"
 
 local _M = {}
 
 function _M.execute(conf)
-  require('mobdebug').start('192.168.2.84')
+  request_path=ngx.var.uri
   for _,id in ipairs(conf.whitelist) do
     pattern=conf.template:gsub("$dataset_id",id)
-    ngx.log(ngx.ERR, pattern)
+    ngx.log(ngx.ERR, "Test request "..request_path.." against "..pattern)
+    if ngx.re.match(request_path,pattern) then
+      ngx.log(ngx.ERR, "Dataset match found for "..request_path)
+    else
+      ngx.log(ngx.ERR, "Dataset match not found for "..request_path)
+      return responses.send_HTTP_FORBIDDEN("Dataset access not authorized")
+    end
   end
 end
 
--- kong/plugins/dataset-access/schema.lua
+++ kong/plugins/dataset-access/schema.lua
@@ -2,7 +2,7 @@ local Errors = require "kong.dao.errors"
 
 return {
   fields = {
-    template = {type = "string", default="/(*.)/$dataset_id"},
+    template = {type = "string", default="/(.*)/$dataset_id"},
     blacklist = {type = "array", default = {}},
     whitelist = {type = "array", default = {}}
   },
@@ -11,6 +11,11 @@ return {
       return false, Errors.schema "You cannot set both a whitelist and a blacklist"
     elseif not (next(plugin_t.whitelist or {}) or next(plugin_t.blacklist or {})) then
       return false, Errors.schema "You must set at least a whitelist or blacklist"
+    end
+    _,id = next(plugin_t.whitelist or plugin_t.blacklist)
+    pattern_check=plugin_t.template:gsub("$dataset_id",id)
+    if ngx.re.match("",pattern_check)==nil then
+      return false, Errors.schema "Syntax error in template"
     end
     return true
   end
